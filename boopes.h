4ac1d9f7a62792a32694090d88b210c236eb3296
Anon Ymous <73802848+ancientstraits@users.noreply.github.com>
2021-10-06 09:02:46 -0400

    Implement a more natural way of class creation
    (Part 1)

--- boopes.h
+++ boopes.h
@@ -3,8 +3,7 @@
 
 #include <stdlib.h>
 
-#define class(ClassName) typedef struct ClassName {
-#define class_end(ClassName) } ClassName ;
+#define class(ClassName) typedef struct ClassName ClassName; struct ClassName
 #define method_proto(ClassName, RetVal, name, ...) RetVal (*name) (struct ClassName*, ##__VA_ARGS__)
 #define method_def(ClassName, RetVal, MethodName, ...) RetVal MethodName (ClassName* this, ##__VA_ARGS__)
 #define inherits_from(ClassName) ClassName super;
