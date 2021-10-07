9e8e3909e290395245e5b3ad043dfaa0b52ba082
Anon Ymous <73802848+ancientstraits@users.noreply.github.com>
2021-10-07 09:05:32 -0400

    Change `inherits_from` to `extends`
    It would be much more familiar to use `extends` instead of `inherits_from`, because several other languages do that, and it is just one word. Although it might not seem like that big of a change, it probably will be in the long term.

--- boopes.h
+++ boopes.h
@@ -7,7 +7,7 @@
 #define class_end(ClassName) } ClassName ;
 #define method_proto(ClassName, RetVal, name, ...) RetVal (*name) (struct ClassName*, ##__VA_ARGS__)
 #define method_def(ClassName, RetVal, MethodName, ...) RetVal MethodName (ClassName* this, ##__VA_ARGS__)
-#define inherits_from(ClassName) ClassName super;
+#define extends(ClassName) ClassName super;
 #define call_method(object, Method, ...) object->Method(object, ##__VA_ARGS__)
 #define new(ClassName) (ClassName*)malloc(sizeof(ClassName))
 #define delete(object) free(object)
